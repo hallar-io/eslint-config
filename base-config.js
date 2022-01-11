d3c0e01516c94317b908186f2596cdd0326364ec
Tom Ploem <tom.ploem@gmail.com>
2022-01-11 20:10:59 +0100

    chore: addition of new rule: no any type usage

--- base-config.js
+++ base-config.js
@@ -23,7 +23,9 @@ module.exports = {
         sourceType: 'module',
       },
       plugins: ['@typescript-eslint'],
-      rules: {},
+      rules: {
+        'no-any': 'off',
+      },
     },
   ],
 };
