4804469ed07a91fd1b37e505f4b23033a98c9209
Tom Ploem <tom.ploem@gmail.com>
2022-01-11 20:15:39 +0100

    fix: move 'no-any' to override in basics

--- base-config.js
+++ base-config.js
@@ -23,9 +23,7 @@ module.exports = {
         sourceType: 'module',
       },
       plugins: ['@typescript-eslint'],
-      rules: {
-        'no-any': 'off',
-      },
+      rules: {},
     },
   ],
 };
